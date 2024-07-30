787681bbff04562b1b7bf6c73bb0c480e18f0887
AntonyM00 <tpumba002@gmail.com>
2024-07-30 22:19:51 +0300

    ..

--- Swap Circle/src/components/Header.jsx
+++ Swap Circle/src/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   const linkClass=({isActive})=>(isActive ? 'm-auto rounded-xl text-white ring-1 ring-white ring-offset-1 p-1':'m-auto rounded-xl text-white p-1')
   return (
     <>
-    <div className='w-full bg-yellow-800 h-14 flex place-items-stretch '>
+    <div className='w-full fixed  bg-yellow-800 h-14 flex place-items-stretch '>
         <NavLink className={linkClass} to='/' >Discover</NavLink>
         {/* <button className=' p-1 rounded-xl text-white focus:ring-1 focus:ring-red-200 ring-offset-1 m-auto' >Team</button> */}
         <NavLink className={linkClass} to='/gallery' >Gallery </NavLink>
--- Swap Circle/src/pages/LandingPage.jsx
+++ Swap Circle/src/pages/LandingPage.jsx
@@ -10,7 +10,7 @@ const LandingPage = () => {
     <div className="bg-[url('/landingCarouselOne.jpg')] w-full h-96 p-2 bg-no-repeat bg-cover bg-center flex flex-col  justify-center  relative">
       <div className='absolute inset-0 bg-black bg-opacity-50 flex flex-col justify-center p-2'>
 
-       <h1 className="text-3xl text-white font-bold underline mb-10  ">SUSTAINABLE FASHION THROUGH SWAPPING</h1>
+       <h1 className=" text-white font-bold underline mb-10 text-4xl ">SUSTAINABLE FASHION THROUGH SWAPPING</h1>
        <h2 className='text-white'>Elevate your wardrobe while saving the planet</h2>
       <button className="text-white border-2 rounded-3xl flex p-1 mb-3 mt-12 w-3/12">See Events
         <GoArrowUpRight/>  
@@ -18,13 +18,17 @@ const LandingPage = () => {
       </div>
        
     </div>
-    <div className='w-full flex place-content-center'>
-      <h3 className='w-10/12'>Events and opportunities  to exchange clothing items with others, fostering a circular economy in fashion.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut suscipit, magna id lobortis pretium, nisl nulla faucibus orci, rhoncus posuere olor</h3>
+    <div className=' flex w-full'><div className='container place-content-center'>
+      <h1 className="text-3xl m-4">Elevate your wardrobe while saving the planet</h1>
+      <h3 className='w-10/12'>Events and opportunities  to exchange clothing items with others, fostering a circular economy in fashion.Discover a world of endless possibilities at Swap Circle, where like-minded individuals come together to share, trade, and connect. Our vibrant community is your go-to platform for swapping everything from fashion and electronics to home goods and collectibles.
+      </h3>
+    <p>Experience the thrill of finding hidden treasures, giving new life to pre-loved items, and reducing your environmental impact. Join the Swap Circle movement today and unlock a sustainable and exciting way to refresh your life!</p>
     </div>
-    <h2 className='text-red-700 text-3xl '>check the gallery page- team page ticket page kinda-done semi-stable</h2>
+    </div>
+    {/* <h2 className='text-red-700 text-3xl '>check the gallery page- team page ticket page kinda-done semi-stable</h2> */}
     <div className="flex flex-wrap">
-  <div className="container flex flex-wrap m-auto">
-    <Link className="m-auto " to="/gallery">
+  <div className="container flex flex-wrap mt-4 ml-auto mr-auto">
+    <Link className="ml-1 mt-3" to="/gallery">
       <div className="bg-[url('/eventimages/IMG_3267.jpg')] bg-no-repeat bg-cover bg-center flex flex-col  justify-center  relative rounded-xl w-56 h-56">
       <div className='absolute inset-0 rounded-xl bg-green-200 bg-opacity-50 flex flex-col justify-center p-2'>
        <h1 className="text-3xl justify-center text-white font-bold underline mb-10  ">Gallery</h1>
@@ -32,14 +36,14 @@ const LandingPage = () => {
       </div>      
     </Link>
     
-    <Link className="m-auto " to="/team">
+    <Link className="ml-5 mt-3 " to="/team">
       <div className="bg-[url('/eventimages/432987732_769818475123330_1646760977378195407_n.jpg')] bg-no-repeat bg-cover bg-center flex flex-col  justify-center  relative rounded-xl w-56 h-56">
       <div className='absolute inset-0 rounded-xl bg-yellow-200 bg-opacity-50 flex flex-col justify-center p-2'>
        <h1 className="text-3xl justify-center text-white font-bold underline mb-10  ">Team</h1>
        </div>
       </div>      
     </Link>
-    <Link className="m-auto " to="/tickets">
+    <Link className="ml-9 mt-3 " to="/tickets">
       <div className="bg-[url('eventimages/IMG_3258.jpg')] bg-no-repeat bg-cover bg-center flex flex-col  justify-center  relative rounded-xl w-56 h-56">
       <div className='absolute inset-0 rounded-xl bg-red-200 bg-opacity-50 flex flex-col justify-center p-2'>
        <h1 className="text-3xl justify-center text-white font-bold underline mb-10  ">Tickets</h1>
